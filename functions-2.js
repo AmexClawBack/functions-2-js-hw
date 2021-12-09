f0ef363f6b23030f1f4d6aed291be72622e173a0
Dillon King <Dillonk2012@gmail.com>
2021-12-08 19:43:01 -0600

    Final commit for javascript homework. All answers correct with a good understanding of each

--- functions-2.js
+++ functions-2.js
@@ -1,6 +1,6 @@
-/* 
-1. Define a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript. Do some googling to figure this out if you forget how conditionals work.
-*/
+// /* 
+// 1. Define a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript. Do some googling to figure this out if you forget how conditionals work.
+// */
 
 maxOfTwoNumbers = (num1, num2) => {
   if (num1 > num2) {
@@ -10,41 +10,40 @@ maxOfTwoNumbers = (num1, num2) => {
   }
 };
 
-console.log(maxOfTwoNumbers(24, 76));   //This is correct
+console.log(maxOfTwoNumbers(24, 76));  
+// // maxOfThree();
 
-// maxOfThree();
-
-/*
-2. Define a function maxOfThree that takes three numbers as arguments and returns the largest of them.
-*/
+// /*
+// 2. Define a function maxOfThree that takes three numbers as arguments and returns the largest of them.
+// */
 maxOfThree = (num1, num2, num3) => {
-  if (num1 > num2 && num3) {
+  if (num1 > (num2 && num3)) {
     return num1;
-  } else if (num2 > num1 && num3) {
+  } else if (num2 > (num1 && num3)) {
     return num2;
   } else {
     return num3;
   }
 };
 
-console.log(maxOfThree(24, 75, 130));   // This is Wrong
+console.log(maxOfThree(2443, 755, 130));   
 
-/*
-3. Write a function isCharacterAVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
-*/
+// /*
+// 3. Write a function isCharacterAVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+// */
 isCharacterAVowel = (str) => {
-  if (str === 'a' || 'i' || 'o' || 'u' || 'e') {
+  if (str === ('a' || 'i' || 'o' || 'u' || 'e')) {
     return true;
   } else {
     return false;
   }
 };
 
-console.log(isCharacterAVowel('l'));    // This is wrong
+console.log(isCharacterAVowel('l'));   
 
-/*
-4. Define a function sumArray and a function multiplyArray that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sumArray([1,2,3,4]) should return 10, and multiplyArray([1,2,3,4]) should return 24.
-*/
+// /*
+// 4. Define a function sumArray and a function multiplyArray that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sumArray([1,2,3,4]) should return 10, and multiplyArray([1,2,3,4]) should return 24.
+// */
 
 sumArray = (numbers) => {
   let total = 0;  // variable to store iteration sums
@@ -68,15 +67,17 @@ multiplyArray = (numbers) => {
 
 console.log(multiplyArray([1,2,3,4])); // dont forget all parenthesis when console logging functions; returns undefined.
 
-/*
-5.Write a function that returns the number of arguments passed to the function when called.
-*/
+// /*
+// 5.Write a function that returns the number of arguments passed to the function when called.
+// */
 
-returnArgument = (number) => {
-  return number.length;
+
+function test(a, e, s, h,s, s){
+  console.log(arguments.length);
 }
+test("4","5",8, 5);
 
-console.log(returnArgument([1,2,3,4,5]));  //Correct, but Double check answer with sheet when done
+
 
 /*
 6. Define a function reverseString that computes the reversal of a string. For example, reverseString("jag testar") should return the string "ratset gaj".
@@ -96,12 +97,28 @@ console.log(reverseString("stanley yelnats"));
 7. Write a function findLongestWord that takes an array of words and returns the length of the longest one.
 */
 findLongestWord = (arr) => {
-
-};
-
+ let longest = arr[0]
+   for (let i = 0; i < arr.length; i++) {
+     if (longest.length < arr[i].length) {
+       longest = arr[i];
+     }
+    
+   }
+   return longest.length;
+ }
+ console.log(findLongestWord(['test', 'nonetheless', 'atomic']))
 /*
 8. Write a function filterLongWords that takes an array of words and a number i and returns the array of words that are longer than i characters long.
 */
-filterLongWords = () => {
-  // ADD YOUR CODE HERE
+filterLongWords = (array, number) => {
+  let finalArray = []; 
+  for(let i = 0 ; i < array.length; i++){
+  if(array[i].length > number){
+  finalArray.push(array[i]);   
+} 
+} 
+return finalArray 
 };
+
+
+console.log(filterLongWords(['tender', 'balogni', 'macaroni', 'hi', 'tenderloin']))
